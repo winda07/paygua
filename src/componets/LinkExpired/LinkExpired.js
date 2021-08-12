dc76eba1f8717be492145a518d30fb2297813fe3
“windaa.1990@gmail.com” <winda.lyfe@gmail.com>
2021-08-13 03:16:24 +0700

    button login success and expired

--- src/componets/LinkExpired/LinkExpired.js
+++ src/componets/LinkExpired/LinkExpired.js
@@ -2,13 +2,17 @@ import React from 'react'
 import styles from "./LinkExpired.module.css"
 import thanks from "../../img/popup-tagihan.svg"
 import success from "../../img/ion.svg"
-import { Redirect, useHistory } from 'react-router-dom'
+import { Link, useHistory } from "react-router-dom";
 const LinkExpired = () => {
     let currentRender = (
         <div className={styles.App}>
             <div className={styles['form-signin']}>
                 <img className={styles.logo} src={success} alt="logo" />
                 <p className={styles.text}>LinkExpired</p>
+                <button className={styles.btnSubmit}>
+                    <Link to="/login" style={{ textDecoration: 'none' }} className={styles.text1}>Login</Link>
+                    {/* <p className={styles.text1}> Masuk</p> */}
+                </button>
             </div>
         </div>
     )
--- src/componets/Success/Succes.js
+++ src/componets/Success/Succes.js
@@ -2,13 +2,17 @@ import React from 'react'
 import styles from "./Succes.module.css"
 import thanks from "../../img/popup-tagihan.svg"
 import success from "../../img/Logo_LupPW.svg"
-import { Redirect, useHistory } from 'react-router-dom'
+import { Link, useHistory } from "react-router-dom";
 const Success = () => {
     let currentRender = (
         <div className={styles.App}>
             <div className={styles['form-signin']}>
                 <img className={styles.logo} src={success} alt="logo" />
                 <p className={styles.text}>Success</p>
+                <button className={styles.btnSubmit}>
+                    <Link to="/login" style={{ textDecoration: 'none' }} className={styles.text1}>Login</Link>
+                    {/* <p className={styles.text1}> Masuk</p> */}
+                </button>
             </div>
         </div>
     )
